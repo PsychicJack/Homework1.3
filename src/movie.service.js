54de43b03bee96efb5563c60ba4ed8609a4cf9b9
Peric Milosevic <supremeruler98@gmail.com>
2020-03-25 20:38:37 +0100

    changed db urls

--- src/movie.service.js
+++ src/movie.service.js
@@ -1,9 +1,9 @@
 import { Movie } from "./movie.js";
 import { Actor } from "./actor.js";
 
-const URL_MOVIES = "https://my-json-server.typicode.com/PsychicJack/Homework_1/movies";
-const URL_ACTORS = "https://my-json-server.typicode.com/PsychicJack/Homework_1/actors";
-const URL_ACTEDIN = "https://my-json-server.typicode.com/PsychicJack/Homework_1/actedIn";
+const URL_MOVIES = "https://my-json-server.typicode.com/PsychicJack/Homework1.5/movies";
+const URL_ACTORS = "https://my-json-server.typicode.com/PsychicJack/Homework1.5/actors";
+const URL_ACTEDIN = "https://my-json-server.typicode.com/PsychicJack/Homework1.5/actedIn";
 
 export function getMovieNames() {
     return new Promise((resolve, reject) => {
